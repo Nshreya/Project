808983ef6ad6eaab3af54ea59c4877ceb5715cc5
Nshreya <nallapu@gmail.com>
2014-06-20 14:30:08 -0400

    first commit

---
+++ src/main/java/com/luckypants/command/CreateBookCommand.java
@@ -0,0 +1,36 @@
+package com.luckypants.command;
+
+import java.util.ArrayList;
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.luckypants.model.Author;
+import com.luckypants.model.Book;
+import com.luckypants.mongo.BooksConnectionProvider;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+public class CreateBookCommand {
+
+	public boolean execute(Book book) {
+		BooksConnectionProvider booksConn = new BooksConnectionProvider();
+		DBCollection booksCollection = booksConn.getCollection();
+
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			DBObject dbObject = (DBObject) JSON.parse(mapper
+					.writeValueAsString(book));
+			booksCollection.insert(dbObject);
+		} catch (Exception e) {
+			System.out.println("ERROR during mapping book to Mongo Object");
+			return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+	
+	}
+}
